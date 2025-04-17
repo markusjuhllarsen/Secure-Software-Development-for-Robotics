f782b17145f5e7f973f0dcbd34e11ecd3f38b6be
Oliver Michaelsen <olmic24@student.sdu.dk>
2025-04-17 19:45:02 +0200

    Update __init__.py

--- controller/__init__.py
+++ controller/__init__.py
@@ -1,8 +1,3 @@
-"""
-Controller package for Secure Turtlebot4 Controller application.
-Contains robot control, security, and action client modules.
-"""
-
 from controller.robot_controller import SecureTurtlebot4Controller
 from controller.security import SecurityManager
 from controller.actions import DockingManager

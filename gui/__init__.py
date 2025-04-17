ae57834b6b2fbdf3e556f1fcd4a233998dfda5ec
Oliver Michaelsen <olmic24@student.sdu.dk>
2025-04-17 19:45:22 +0200

    Update __init__.py

--- gui/__init__.py
+++ gui/__init__.py
@@ -1,8 +1,3 @@
-"""
-GUI package for Secure Turtlebot4 Controller application.
-Contains the main GUI application and widget components.
-"""
-
 from gui.gui_app import ButtonControlGUI
 
 __all__ = ['ButtonControlGUI']
